41a460d7d3d936ecbfc58c157a5a1cdda66507af
Code Nation 18 <codenation@CN-18.lan>
2019-03-19 10:32:06 +0000

    heroku

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const geocode = require('./geocode')
+const forecast = require('./forecast')
+
+
+const path = require('path')
+
+
+const app = express();
+const publicDirectory = path.join(__dirname, '../public')
+
+
+app.use(express.static(publicDirectory))
+
+  app.get('/weather', (req, res) => {
+    if(!req.query.address){
+      return res.send ('Please Search for an address')
+    }
+  
+    geocode (req.query.address, (error, response) => {
+      if (error){
+        return res.send(error)
+      }
+  
+  forecast(response.latitude, response.longitude, (error, forecastData) => {
+    if (error) {
+      return res.send (error)
+    }
+  
+    res.send({
+      forecast: forecastData,
+      location: response.location,
+      address: req.query.address
+    })
+  
+  })
+  }) 
+})
+app.listen(3000,() => { 
+    console.log('Server is up on port 3000') 
+})
